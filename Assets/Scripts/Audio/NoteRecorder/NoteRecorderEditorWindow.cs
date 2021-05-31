77b4645773873569caf57422b669806acdb22bab
TakaBro <lukastakatani@gmail.com>
2021-05-31 19:56:25 -0400

    Create note recorder editor window

---
+++ Assets/Scripts/Audio/NoteRecorder/NoteRecorderEditorWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NoteRecorderEditorWindow : EditorWindow
+{
+    [MenuItem("Window/NoteRecorder")]
+    public static void ShowWindow()
+    {
+        GetWindow<NoteRecorderEditorWindow>();
+    }
+
+    private void OnGUI()
+    {
+        //Window Code
+    }
+}
