18a714543c3b0604cee889a84f31961d0439691a
TakaBro <lukastakatani@gmail.com>
2021-06-04 19:03:26 -0400

    Create basic GUI on Note Recorder Editor Window

---
+++ Assets/Scripts/Audio/NoteRecorder/NoteRecorderEditorInspector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+public class NoteRecorderEditorInspector : MonoBehaviour
+{
+    void OnGUI()
+    {
+        /*GUISkin skin = GUI.skin;
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("1", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Button("2", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("3", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("4", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("5", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+
+        GUILayout.EndHorizontal();*/
+    }
+}
--- Assets/Scripts/Audio/NoteRecorder/NoteRecorderEditorWindow.cs
+++ Assets/Scripts/Audio/NoteRecorder/NoteRecorderEditorWindow.cs
@@ -11,8 +11,62 @@ public class NoteRecorderEditorWindow : EditorWindow
         GetWindow<NoteRecorderEditorWindow>();
     }
 
-    private void OnGUI()
+    void OnGUI()
     {
-        //Window Code
+        GUISkin skin = GUI.skin;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("0.5", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("1.0", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("1.5", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("2.0", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("2.5", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Label("3.0", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Note 1", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Note 2", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Note 3", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Note 4", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("X", GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.Button("", skin.label, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(50f), GUILayout.MaxWidth(50f));
+        GUILayout.EndHorizontal();
     }
 }
